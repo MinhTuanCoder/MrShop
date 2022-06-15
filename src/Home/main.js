75992799e4dec99ebe4edb715f5d9db99d4d71f0
MinhTuanCoder <86965221+MinhTuanCoder@users.noreply.github.com>
2022-06-15 22:13:25 +0700

    cap nhat mot ty

--- src/Home/main.js
+++ src/Home/main.js
@@ -3,6 +3,7 @@ const bodyOverlay = document.getElementById('Body_overlay');
 const colLeft = document.getElementById('col-left');
 const menuButton = document.getElementById('js-menu-toggle');
 const closeButton = document.getElementById("btn-close");
+const popLogin = document.getElementById('m_login');
 
 
 menuButton.addEventListener('click', (event) => {
@@ -18,4 +19,7 @@ closeButton.addEventListener('click',function(e){
 bodyOverlay.addEventListener('click', function(e){
 	bodyOverlay.classList.add("d-none");
 	colLeft.classList.remove("active");
+})
+popLogin.addEventListener('click',function(e){
+	popLogin.classList.add('popShow')
 })
