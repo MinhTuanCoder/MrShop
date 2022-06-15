77bf17a18d44e5993ad554bec239d7ed36dc84d9
MinhTuanCoder <86965221+MinhTuanCoder@users.noreply.github.com>
2022-06-15 13:35:46 +0700

    update navbar left using javascrip

--- src/Home/main.js
+++ src/Home/main.js
@@ -1,17 +1,21 @@
 
-
-
+const bodyOverlay = document.getElementById('Body_overlay');
+const colLeft = document.getElementById('col-left');
 const menuButton = document.getElementById('js-menu-toggle');
-const menumbButton = document.getElementById('js-menu-toggle-mobile');
-if (menuButton && colLeft){
-	menuButton.addEventListener('click', (event) => {
-		colLeft.classList.toggle("active");
-		bodyOverlay.classList.toggle("d-none")
-	})
-}
-if (menumbButton && colLeft){
-	menumbButton.addEventListener('click', (event) => {
-		colLeft.classList.toggle("active");
-		bodyOverlay.classList.toggle("d-none")
-	})
-}
+const closeButton = document.getElementById("btn-close");
+
+
+menuButton.addEventListener('click', (event) => {
+		colLeft.classList.add("active");
+		bodyOverlay.classList.remove("d-none")
+})
+
+
+closeButton.addEventListener('click',function(e){
+    bodyOverlay.classList.add("d-none");
+	colLeft.classList.remove("active");
+})
+bodyOverlay.addEventListener('click', function(e){
+	bodyOverlay.classList.add("d-none");
+	colLeft.classList.remove("active");
+})
