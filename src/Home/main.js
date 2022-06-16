f4443b152f1995d7aad4e680f53d29118072d1c1
MinhTuanCoder <86965221+MinhTuanCoder@users.noreply.github.com>
2022-06-16 22:34:29 +0700

    them animation cho nut dang nhap

--- src/Home/main.js
+++ src/Home/main.js
@@ -3,6 +3,7 @@ const bodyOverlay = document.getElementById('Body_overlay');
 const colLeft = document.getElementById('col-left');
 const menuButton = document.getElementById('js-menu-toggle');
 const closeButton = document.getElementById("btn-close");
+const loginButton = document.getElementById('js-login-toggle');
 const popLogin = document.getElementById('m_login');
 
 
@@ -20,6 +21,20 @@ bodyOverlay.addEventListener('click', function(e){
 	bodyOverlay.classList.add("d-none");
 	colLeft.classList.remove("active");
 })
-popLogin.addEventListener('click',function(e){
-	popLogin.classList.add('popShow')
+
+loginButton.addEventListener('click', (event)=>{
+	if(popLogin.classList.contains('popShow')){
+		popLogin.classList.remove('popShow');
+	colLeft.classList.remove("active");
+	bodyOverlay.classList.add("d-none");
+	}
+	else{
+		popLogin.classList.add('popShow');
+	colLeft.classList.remove("active");
+	bodyOverlay.classList.add("d-none");
+	}
+	
+	
 })
+
+
